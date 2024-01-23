fc325d734904843514451f0b757b27f59ecfdeb1
Daniel Vieira <dann-vieira@hotmail.com>
2024-01-22 22:32:04 -0300

    feat: login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import { ModeToggle } from "@/components/ModeToggle";
+import LoginForm from "@/components/LoginForm";
+import LoginIllustration from "@/components/assets/LoginIllustration";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <ModeToggle />
+    <div className="flex items-center">
+      <LoginIllustration />
+
+      <div className="border-2 rounded-lg w-96 h-80 p-6 mx-auto flex flex-col justify-evenly bg-gray-700">
+        <LoginForm />
+        <hr className="border-gray-500" />
+        <Button className="w-48 self-center">Criar nova conta</Button>
+      </div>
     </div>
   );
 }
