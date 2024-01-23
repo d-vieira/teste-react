0e8a087014089ab6ad32c0286e2c6587fc59a22e
Daniel Vieira <dann-vieira@hotmail.com>
2024-01-23 20:30:14 -0300

    feat: Register Customer Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Header from "@/components/Header";
+
 export default function Home() {
-  return <h1>theres nothing here... yet</h1>;
+  return <Header />;
 }
---
+++ src/app/register-customer/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+import RegisterClienteForm from "@/components/RegisterClientForm";
+
+export default function RegisterCustomer() {
+  return (
+    <div className="flex flex-col">
+      <Header />
+      <RegisterClienteForm />
+    </div>
+  );
+}
