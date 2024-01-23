1871ece3db74807b74d25e537c029c0d591e62a5
Daniel Vieira <dann-vieira@hotmail.com>
2024-01-23 01:06:32 -0300

    feat: Sign up form component

---
+++ src/components/SignUpForm.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Form, FormControl, FormField, FormItem } from "./ui/form";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { ChangeEvent } from "react";
+
+const signUpSchema = z.object({
+  username: z.string().min(1),
+  email: z.string().email().min(1).toLowerCase(),
+  password: z.string().min(6),
+  phone: z.string().min(12),
+  image: z.string(),
+});
+
+type SignUpFormProps = {
+  handleImagePreview: (image: string) => void;
+};
+
+export default function SignUpForm({ handleImagePreview }: SignUpFormProps) {
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      email: "",
+      username: "",
+      password: "",
+      phone: "",
+      image: "",
+    },
+  });
+
+  function handleFileChange(e: ChangeEvent<HTMLInputElement>) {
+    if (e.target.files) {
+      const reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onloadend = () => {
+        handleImagePreview(reader.result as string);
+      };
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <FormField
+        control={form.control}
+        name="email"
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <Input
+                className="dark:text-black/80 placeholder:text-black/60"
+                placeholder="Email"
+                {...field}
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+
+      <FormField
+        control={form.control}
+        name="username"
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <Input
+                className="dark:text-black/80 placeholder:text-black/60"
+                placeholder="Nome de Usuário"
+                {...field}
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="phone"
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <Input
+                className="dark:text-black/80 placeholder:text-black/60"
+                placeholder="Número de Telefone"
+                {...field}
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="password"
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <Input
+                type="password"
+                className="dark:text-black/80 placeholder:text-black/60"
+                placeholder="Senha"
+                {...field}
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+      <input
+        type="file"
+        name="image"
+        accept="image/*"
+        className="block w-full text-sm text-slate-500
+                file:mr-4 file:py-2 file:px-4
+                file:rounded-full file:border-0
+                file:text-sm file:font-semibold
+                file:bg-zinc-200 file:text-black/80
+                hover:file:bg-zinc-50"
+        onChange={handleFileChange}
+      />
+
+      <Button
+        type="submit"
+        className="text-lg text-black/70 bg-emerald-400 hover:bg-emerald-950 hover:text-white"
+      >
+        Cadastrar Usuário
+      </Button>
+    </Form>
+  );
+}
