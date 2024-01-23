d0dbbcb6f0df227ed514a58b3f4902bfe6dd6db2
Daniel Vieira <dann-vieira@hotmail.com>
2024-01-23 20:24:22 -0300

    feat: ProfileMenu Component

---
+++ src/components/ProfileMenu.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "./ui/menubar";
+
+export default function ProfileMenu() {
+  return (
+    <Menubar className="h-14">
+      <MenubarMenu>
+        <MenubarTrigger>
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <MenubarContent>
+              <Link href={"/login"}>
+                <MenubarItem>Sair</MenubarItem>
+              </Link>
+            </MenubarContent>
+          </Avatar>
+        </MenubarTrigger>
+      </MenubarMenu>
+    </Menubar>
+  );
+}
